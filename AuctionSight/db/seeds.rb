7b45f4f859c9fcd645fd72868dc1c7ed7a8b6340
John <johnconley507@hotmail.com>
2016-07-25 09:45:47 -0400

    github bug fixes

---
+++ AuctionSight/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!(name: "Sammy Davis Junior", email: "hipcat@crooner.com")
+User.create!(name: "Frank Sinatra", email: "doobiedo@loundelizard.com")
+User.create!(name: "Dean Martin", email: "deano@borracho.com")
